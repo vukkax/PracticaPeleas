898377d22b69bff7cd446e3360d0838a0d61808b
Insideab <heynoobz4@gmail.com>
2025-12-17 17:29:30 +0100

    Healthbars funcionan ahora

--- ProyectoPeleasDiseño/Assets/Scripts/UI/LerpSlider.cs
+++ ProyectoPeleasDiseño/Assets/Scripts/UI/LerpSlider.cs
@@ -1,24 +1,25 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class LerpSlider : MonoBehaviour
 {
-    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Slider otherSlider;
     private Slider m_lerpSlider;
     
     void Start()
     {
         m_lerpSlider = GetComponent<Slider>();
-        m_lerpSlider.maxValue = healthSlider.maxValue;
-        m_lerpSlider.value = healthSlider.value;
+        m_lerpSlider.maxValue = otherSlider.maxValue;
+        m_lerpSlider.value = otherSlider.value;
     }
     
     void Update()
     {
-        m_lerpSlider.maxValue = healthSlider.maxValue;
-        if (m_lerpSlider.value != healthSlider.maxValue)
+        m_lerpSlider.maxValue = otherSlider.maxValue;
+        if (m_lerpSlider.value != otherSlider.value)
         {
-            m_lerpSlider.value = Mathf.Lerp(m_lerpSlider.value, healthSlider.value, 1.5f * Time.deltaTime);
+            m_lerpSlider.value = Mathf.Lerp(m_lerpSlider.value, otherSlider.value, 1.5f * Time.deltaTime);
         }
     }
 }
